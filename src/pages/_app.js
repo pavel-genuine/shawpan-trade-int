bf8b4dfdf90cab1fe16c64776a573f7a703fab70
pavel.genuine <pavel.playstore@gmail.com>
2023-05-05 05:53:31 +0600

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
+import { useState } from 'react'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const [isHome,setIsHome]=useState(false)
+
+  return <Component setIsHome={setIsHome} {...pageProps} />
 }
