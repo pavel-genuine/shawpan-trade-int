1b55f418554f971136948d4b67648bdc7b8b4871
pavel.genuine <pavel.playstore@gmail.com>
2023-05-18 10:57:01 +0600

    qc

--- src/Components/Qualities/Qualities.js
+++ src/Components/Qualities/Qualities.js
@@ -11,7 +11,7 @@ import { motion } from "framer-motion"
 
 const Qualities = () => {
     return (
-        <div className='lg:flex lg:h-[90vh] bg-bg lg:py-[7vh] py-5 text-[white] overflow-hidden  lg:ml-0 px-[5vw]'>
+        <div className='lg:flex lg:h-[90vh] bg-bg lg:py-[7vh] py-5 text-[white] overflow-hidden  lg:ml-0 pl-[20.5px] lg:px-[5vw]'>
 
             <motion.div
                 viewport={{ once: true }}
@@ -23,9 +23,9 @@ const Qualities = () => {
 
                     <div className='flex lg:mb-[5vh] mb-5 '>
 
-                        <div className=' bg-[#1F2937]  mr-5 lg:mr-[5vh] p-5 lg:h-[36vh] h-[40vh] overflow-hidden lg:w-[25vw] w-[42vw]'>
+                        <div className=' bg-[#1F2937] mr-5 lg:mr-[5vh] lg:p-5 p-3 lg:h-[36vh] h-[250px] overflow-hidden lg:w-[25vw] w-[42vw]'>
                             <LazyLoadImage
-                                className={`w-[100px]`}
+                                className={`lg:w-[80px] w-[70px]`}
                                 alt={'cate'}
                                 // height={500}
                                 // width={900}
@@ -34,29 +34,29 @@ const Qualities = () => {
                             >
 
                             </LazyLoadImage>
-                            <h1 className='text-xl my-4'>
+                            <h1 className='lg:text-xl lg:my-3 my-2'>
                                 Excellence
                             </h1>
-                            <p className='font-thin text-sm lg:text-md'>
+                            <p className='font-thin text-sm lg:text-lg'>
                                 Committed to excellence and tackling challenges with a can-do Attitude at Shwapan Trade International.
                             </p>
                         </div>
 
-                        <div className=' bg-bg2 p-5 lg:h-[36vh] h-[40vh] overflow-hidden lg:w-[25vw] w-[42vw]'>
+                        <div className=' bg-bg2 mr-5 lg:mr-[5vh] lg:p-5 p-3 lg:h-[36vh] h-[250px] overflow-hidden lg:w-[25vw] w-[42vw]'>
                             <LazyLoadImage
-                                className={`w-[120px]`}
+                                className={`lg:w-[100px] w-[70px]`}
                                 alt={'cate'}
-                                height={500}
-                                width={900}
+                                // height={500}
+                                // width={900}
                                 src={img2.src}
 
                             >
 
                             </LazyLoadImage>
-                            <h1 className='text-xl my-4'>
+                            <h1 className='lg:text-xl lg:my-3 my-2'>
                                 Quality
                             </h1>
-                            <p className='font-thin text-sm lg:text-md'>
+                            <p className='font-thin text-sm lg:text-lg'>
                                 Our fabrics are carefully selected for their durability, comfort, and excellent drape.                    </p>
                         </div>
 
@@ -64,9 +64,9 @@ const Qualities = () => {
 
                     <div className='flex'>
 
-                        <div className='bg-bg2 mr-5 lg:mr-[5vh] p-5 lg:h-[36vh] h-[40vh] overflow-hidden lg:w-[25vw] w-[42vw]'>
+                        <div className=' bg-bg2 mr-5 lg:mr-[5vh] lg:p-5 p-3 lg:h-[36vh] h-[250px] overflow-hidden lg:w-[25vw] w-[42vw]'>
                             <LazyLoadImage
-                                className={`w-[100px]`}
+                                className={`lg:w-[100px] w-[70px]`}
                                 alt={'cate'}
                                 // height={500}
                                 // width={900}
@@ -75,17 +75,17 @@ const Qualities = () => {
                             >
 
                             </LazyLoadImage>
-                            <h1 className='text-xl my-4'>
+                            <h1 className='lg:text-xl lg:my-3 my-2'>
                                 1% Better Everyday
                             </h1>
-                            <p className='font-thin text-sm lg:text-md'>
+                            <p className='font-thin text-sm lg:text-lg'>
                                 Committed to excellence and tackling challenges with a can-do attitude here.
                             </p>
                         </div>
 
-                        <div className='bg-[#1F2937] p-5 lg:h-[36vh] h-[40vh] overflow-hidden lg:w-[25vw] w-[42vw]'>
+                        <div className=' bg-[#1F2937] mr-5 lg:mr-[5vh] lg:p-5 p-3 lg:h-[36vh] h-[250px] overflow-hidden lg:w-[25vw] w-[42vw]'>
                             <LazyLoadImage
-                                className={`w-[100px]`}
+                                className={`lg:w-[100px] w-[70px]`}
                                 alt={'cate'}
                                 // height={500}
                                 // width={900}
@@ -93,11 +93,11 @@ const Qualities = () => {
                             >
 
                             </LazyLoadImage>
-                            <h1 className='text-xl my-4'>
+                            <h1 className='lg:text-xl lg:my-3 my-2'>
                                 Recognition
                             </h1>
-                            <p className='font-thin text-sm lg:text-md'>
-                                At  Shwapan Trade International, we move quickly, operate with urgency, and embrace daily progress.                    </p>
+                            <p className='font-thin text-sm lg:text-lg'>
+                                At Shwapan Trade International, we move quickly, operate with urgency, and embrace daily progress.                    </p>
                         </div>
 
                     </div>
@@ -107,14 +107,14 @@ const Qualities = () => {
 
 
             <motion.div
-                className={`lg:h-[77vh] lg:w-[50%] mt-5 overflow-hidden  lg:mt-0 grayscale`}
+                className={`lg:h-[77vh] lg:w-[50%] mt-5 overflow-hidden  lg:mt-0 grayscale pr-[20px] lg:pr-0`}
                 viewport={{ once: true }}
                 initial={{ opacity: .5, scale: .8 }}
                 whileInView={{ opacity: 1, scale: 1 }}
                 transition={{ duration: .8, delay: 0 }}
             >
                 <LazyLoadImage
-                    className={` grayscale`}
+                    className={` grayscale h-[300px] lg:h-auto w-[100%] object-cover`}
                     alt={'cate'}
                     // height={200}
                     // width={300}
