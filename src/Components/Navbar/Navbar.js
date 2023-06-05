1b2576d3ed317bf19e543640d6ab99215790a65d
pavel.genuine <pavel.playstore@gmail.com>
2023-06-05 15:27:52 +0600

    br

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -91,7 +91,7 @@ const Navbar = () => {
                             <p
                                 className='text-[white] lg:mx-[-4vw] mx-2 inline lg:block lg:mx-0  grayscale bg-[white] inline px-5 py-2 rounded-full bg-opacity-70'
                             >
-                                SwapanTrade International
+                                Swapan Trade International
                             </p>
 
                         </Link>
