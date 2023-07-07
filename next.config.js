c3463ac78029b4bc74e9bacf60423bf85241e177
pavel.genuine <pavel.playstore@gmail.com>
2023-07-07 22:49:55 +0600

    mui/styles

--- next.config.js
+++ next.config.js
@@ -38,6 +38,9 @@ module.exportsg = {
       '@mui/material': {
         transform: '@mui/material/{{member}}'
       },
+      '@mui/styles': {
+        transform: '@mui/styles/{{member}}'
+      },
       '@mui/lab': {
         transform: '@mui/lab/{{member}}'
       },
